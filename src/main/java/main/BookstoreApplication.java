52495a9de9cb7c6c1af72f095107f3d067dd7380
Mirza Adil <madil_mirza@hotmail.com>
2021-04-06 15:01:58 -0700

    Add New Carsual effect & Feature Book

--- src/main/java/main/BookstoreApplication.java
+++ src/main/java/main/BookstoreApplication.java
@@ -1,9 +1,13 @@
-package com.bppkstore;
+package main;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"main.controller"})
+
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/main/controller/HomeController.java
+++ src/main/java/main/controller/HomeController.java
@@ -1,12 +1,12 @@
-package com.bppkstore.controller;
+package main.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping("/controller")
 	public String index() {
 		return "index";
 	}
